55e18b814d6972612af0074d9ff3118a859b487e
Your Name <AggressivelyMeows@users.noreply.github.com>
2022-07-04 21:56:48 +0000

    Fix prebuild.js not supporting replaceAll

--- prebuild.js
+++ prebuild.js
@@ -1,5 +1,6 @@
 var axios = require('axios').default;
 const fs = require('fs')
+const replaceAll = require('string.prototype.replaceall');
 
 const options = {
 	url: 'https://horseman.ceru.dev/v1',
@@ -9,6 +10,8 @@ const options = {
 const process = async () => {
 	const all_posts = await axios.get(`${options.url}/models/posts/objects?key=${options.token}&expand=Author&limit=1000`)
 	console.log(`${options.url}/models/posts/objects?key=${options.token}&expand=Author&limit=1000`)
+
+	console.log(all_posts)
 
 	await fs.promises.mkdir('./data/posts', { recursive: true })
 	await fs.promises.mkdir('./content/posts', { recursive: true })
@@ -28,7 +31,7 @@ const process = async () => {
 		const metadata = Object.assign({}, post)
 		delete metadata.Content
 
-		metadata.slug = post.Title.toLowerCase().replace(/[^a-z0-9]/g, '-').replaceAll('--', '-')
+		metadata.slug = replaceAll(post.Title.toLowerCase().replace(/[^a-z0-9]/g, '-'), '--', '-')
 		metadata.date = post.metadata.created_at
 
 		fs.writeFile(`./content/posts/${post.id}.html`, `${JSON.stringify(metadata)}\n${post.Content}`, () => {})
